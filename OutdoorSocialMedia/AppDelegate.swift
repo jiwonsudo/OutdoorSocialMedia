825b92560d14ec61ea57cad73e07df8779a35196
jiwonsudo <wondev112@gmail.com>
2022-06-01 01:33:02 +0900

    앱 기본세팅

--- OutdoorSocialMedia/AppDelegate.swift
+++ OutdoorSocialMedia/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 0.8)
         return true
     }
 
@@ -36,6 +37,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             // 세로방향 고정
             return UIInterfaceOrientationMask.portrait
         }
-
 }
 
